a07cf412cf0c64c1ffa2dabce8e25aca608f0315
abhaysharma02 <abhas1357@gmail.com>
2026-03-05 02:15:14 +0530

    Migrate Prisma to PostgreSQL and prepare Render configs

--- backend/src/main/java/com/nestely/pos/dto/CategoryRequest.java
+++
@@ -1,9 +0,0 @@
-package com.nestely.pos.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoryRequest {
-    private String name;
-    private boolean active;
-}
--- backend/src/main/java/com/nestely/pos/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.nestely.pos.model;
-
-public enum Role {
-    ADMIN,
-    BILLING,
-    KITCHEN
-}
